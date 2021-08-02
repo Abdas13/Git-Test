d8bbd7375c94803834c696b4a57499992a414b89
Abdas13 <88287139+Abdas13@users.noreply.github.com>
2021-08-02 00:53:14 -0400

    Update Deneme.java

--- src/Deneme.java
+++ src/Deneme.java
@@ -2,5 +2,7 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("merhaba git");
         System.out.println("Ben geldim.");
+        System.out.println("Aaaaaaaaa.");
+        
     }
 }
